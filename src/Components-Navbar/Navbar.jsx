f3c7df8f406da96a1ca4fc59304a11e852ad83a7
Enzo Agu <enzo.gustavo.agu@gmail.com>
2021-10-09 17:27:44 -0300

    desafio-navbar

---
+++ src/Components-Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+
+import '../App.css';
+
+export const Navbar =()=>{
+       
+      return(
+          <>
+          <h1>Tienda MMA </h1>
+          <div className="navbar">
+          <button>Home</button>
+          <button>Guantes</button>
+          <button>Bermudas</button>
+          <button>Lycras</button>
+          <button>Contacto</button>
+          </div>
+          
+      </>
+      )
+
+      
+
+}
